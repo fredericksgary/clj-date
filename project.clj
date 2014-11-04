428fadf4db7365fd4b7bb3f364632b60099c6361
Gary Fredericks <fredericksgary@gmail.com>
2014-11-03 19:42:58 -0600

    Setup version for `lein release`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/clj-date "0.1.1"
+(defproject com.gfredericks/clj-date "0.1.2-SNAPSHOT"
   :description "A Clojure library for timeless dates."
   :url "https://github.com/fredericksgary/clj-date"
   :license {:name "Eclipse Public License"
